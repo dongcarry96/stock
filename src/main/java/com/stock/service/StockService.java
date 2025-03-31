c3414ca940fa6996e28dff7d8826f5baaea54ef0
dong <fhakwpak6@gmail.com>
2025-03-31 18:13:52 +0900

    feat : 재고 감소 로직

---
+++ src/main/java/com/stock/service/StockService.java
@@ -0,0 +1,30 @@
+package com.stock.service;
+
+import com.stock.domain.Stock;
+import com.stock.repository.StockRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StockService {
+
+    private final StockRepository stockRepository;
+
+    // 생성자 생성
+    public StockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    // 재고감소 메소드
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+        // Stock 조회
+        // 재고를 감소시킨뒤
+        // 갱신된 값을 저장하도록
+        Stock stock = stockRepository.findById(id).orElseThrow();
+        stock.decrease(quantity);
+        stockRepository.saveAndFlush(stock);
+    }
+
+
+}
